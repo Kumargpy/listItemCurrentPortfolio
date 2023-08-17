9820b4873ae3b2492493b9d4565f7448dfb05997
Kumargpy <139214726+Kumargpy@users.noreply.github.com>
2023-08-17 15:22:43 +0530

    2 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,13 +5,6 @@ export const HEROES: Hero[] = [
   { icon:'https://placehold.co/40x40', content:'Environment (General)', values:0},
   { icon:'https://placehold.co/40x40', content:'Energy & Efficiency', values:24},
   { icon:'https://placehold.co/40x40', content:'Conservation', values:0 },
-  { icon:'https://placehold.co/40x40', content:'Corporate Social Responsibility', values:0},
-  
+  { icon:'https://placehold.co/40x40', content:'Corporate Social Responsibility', values:0},  
 ];
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
