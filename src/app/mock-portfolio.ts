862e97a16e45453626791eaaa47b3a648ad25f5a
Kumargpy <139214726+Kumargpy@users.noreply.github.com>
2023-08-17 20:26:30 +0530

    1 file modified

--- src/app/mock-portfolio.ts
+++ src/app/mock-portfolio.ts
@@ -4,7 +4,6 @@ export const PORTFOLIO: Portfolio[] = [
   { icon:'https://placehold.co/40x40', content:'Corporate Social Responsibility', values:0},
   { icon:'https://placehold.co/40x40', content:'Environment (General)', values:0},
   { icon:'https://placehold.co/40x40', content:'Energy & Efficiency', values:24},
-  { icon:'https://placehold.co/40x40', content:'Conservation', values:0 },
-  
+  { icon:'https://placehold.co/40x40', content:'Conservation', values:0 },  
 ];
 
