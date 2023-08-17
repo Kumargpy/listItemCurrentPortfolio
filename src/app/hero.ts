e1cb869b113d9d906d2d9006db411ecaa690005c
Kumargpy <139214726+Kumargpy@users.noreply.github.com>
2023-08-17 15:20:28 +0530

    5 files modified

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,6 +1,8 @@
 export interface Hero {
-  id: number;
-  name: string;
+  icon:string;
+  content:string;
+  values:number;
+
 }
 
 
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,15 +1,12 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { icon:'https://placehold.co/40x40', content:'Corporate Social Responsibility', values:0},
+  { icon:'https://placehold.co/40x40', content:'Environment (General)', values:0},
+  { icon:'https://placehold.co/40x40', content:'Energy & Efficiency', values:24},
+  { icon:'https://placehold.co/40x40', content:'Conservation', values:0 },
+  { icon:'https://placehold.co/40x40', content:'Corporate Social Responsibility', values:0},
+  
 ];
 
 
